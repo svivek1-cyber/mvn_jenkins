38d260214e9931ce8c646a99bedae728bdc6b079
svive <svive@X-thing.mshome.net>
2026-03-23 15:23:05 +0530

    first commit

---
+++ MavenJenkins/src/test/java/demo/DemoTest.java
@@ -0,0 +1,11 @@
+package demo;
+
+import org.testng.annotations.Test;
+
+public class DemoTest {
+	@Test
+	public static void main(String[] args) {
+		System.out.println("Build is success");
+	}
+
+}
