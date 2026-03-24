3385586cdee09b4c581c8ce4b4c12d4e19a06a74
svive <svive@X-thing>
2026-03-24 18:53:56 +0530

    qwert commit

--- MavenJenkins/src/test/java/demo/Demo2Test.java
+++ MavenJenkins/src/test/java/demo/Demo2Test.java
@@ -13,5 +13,6 @@ public class Demo2Test {
 		driver.get("https://github.com/svivek1-cyber/ninza_crm_test/blob/master/ninza_crm/pom.xml");
 		Thread.sleep(1);
 		driver.quit();
+		System.out.println("hello i m saurabh");
 	}
 }
--- MavenJenkins/src/test/java/demo/DemoTest.java
+++ MavenJenkins/src/test/java/demo/DemoTest.java
@@ -1,11 +1,19 @@
 package demo;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
 public class DemoTest {
 	@Test
-	public static void main(String[] args) {
-		System.out.println("Build is success");
+	public static void main(String[] args) throws InterruptedException {
+		System.out.println("hello");
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://github.com/svivek1-cyber/ninza_crm_test/blob/master/ninza_crm/pom.xml");
+		Thread.sleep(1);
+		driver.quit();
+		System.out.println("hello i m saurabh");
 	}
 
 }
