cf402cbcc7d2c7265112df1ce877bc625ca8faed
svive <svive@X-thing>
2026-03-26 02:13:42 +0530

    adding ninza crm project to it

---
+++ MavenJenkins/src/test/java/Modules/ContactsTest.java
@@ -0,0 +1,47 @@
+package Modules;
+
+import java.io.IOException;
+import java.time.Duration;
+import java.util.Set;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import generic_utility.BaseClass;
+import object_repository.Campaigns;
+import object_repository.Contacts;
+import object_repository.CreateContact;
+import object_repository.SelectCampaign;
+
+public class ContactsTest extends BaseClass{
+    @Test
+    public void createContactTest() throws IOException { 
+        // CampaignsTest campTest = new CampaignsTest();
+        // campTest.createCampaignTest();
+
+        // driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(30));
+        
+        Campaigns cp = new Campaigns(driver);
+        cp.getContactsLink().click();
+
+        Contacts ct = new Contacts(driver);
+        ct.getCreateContactBtn().click();
+
+        CreateContact cc = new CreateContact(driver);
+        cc.createContact();
+
+        WebElement alert = cp.getAlert();
+        // alert = cp.getAlert();
+        mwdu.waitForElementToBeVisible(alert);
+        if(cp.getAlert().getText().contains("Successfully Added")) {
+            System.out.println("Contact created successfully");
+        }else if(cp.getAlert().getText().contains("already exists")) {
+            System.err.println("Contact already exists");
+        } else {
+            Assert.fail("Failed to create contact");
+        }
+        alert.click();
+    }
+}
